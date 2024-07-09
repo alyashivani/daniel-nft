d4bac9eee080c6c8836d679823da6eba247616cd
bhawnap <bhawnap@witmates.com>
2024-07-09 11:48:16 +0530

    test changes

--- template-app/db/seeds.rb
+++ template-app/db/seeds.rb
@@ -5,6 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+
+
 #============Admin User====================
 
 unless AdminUser.last.present?
